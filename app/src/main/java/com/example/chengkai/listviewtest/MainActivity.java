cd967c4d20212f640f1a731b1415ca9464315c8b
成凯 <hellochengkai@hotmail.com>
2018-03-15 22:51:34 +0800

    添加点击事件

--- app/src/main/java/com/example/chengkai/listviewtest/MainActivity.java
+++ app/src/main/java/com/example/chengkai/listviewtest/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.chengkai.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView = null;
+    private static final String TAG = "MainActivity";
     String [] data = {"text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text","text"};
-    List myData = null;
+    List<MyData> myDatas = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +24,12 @@ public class MainActivity extends AppCompatActivity {
         initview();
     }
     void initdata(){
-        myData = new ArrayList();
+        myDatas = new ArrayList();
         for (int i = 0; i< 20;i ++){
             if(i%2 == 0)
-                myData.add(new MyData("I == " + i,R.mipmap.ic_launcher));
+                myDatas.add(new MyData("I == " + i,R.mipmap.ic_launcher));
             else
-                myData.add(new MyData("I == " + i,R.mipmap.ic_launcher_round));
+                myDatas.add(new MyData("I == " + i,R.mipmap.ic_launcher_round));
         }
     }
     void initview()
@@ -33,8 +37,16 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter arrayAdapter = new MyAdapter(
                 getApplicationContext(),
                 R.layout.list_view_text,
-                myData);
+                myDatas);
         listView = findViewById(R.id.listview);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                MyData myData = myDatas.get(position);
+                Log.d(TAG, "onItemClick: " + myData.getText());
+                Toast.makeText(MainActivity.this,myData.text,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
